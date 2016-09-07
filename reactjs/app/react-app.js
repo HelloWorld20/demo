cbcd3663bb414c9e93a48ce1a13fd356beb2f79e
HelloWorld20 <292701515@qq.com>
2016-09-07 17:53:40 +0800

    commit

--- reactjs/app/react-app.js
+++ reactjs/app/react-app.js
@@ -1,9 +1,28 @@
 var React = require('react');
 var reactDOM = require('react-dom');
+var Collection = require('./collection.react');
 
 var HelloMessage = React.createClass({
+	getInitialState: function() {
+		return {
+			isHidden: true,
+			title: 1
+		};
+	},
+	handleClick: function() {
+		this.setState({
+			isHidden: !this.state.isHidden,
+			title: ++this.state.title
+		})
+	},
 	render: function(){
-		return <h1>Hello {this.props.name}</h1>
+		var headerElement = React.createElement('h1', {className: 'header', key: 'header'}, this.state.title);
+		var buttonElement = React.createElement('button', {key: 'button', onClick: this.handleClick}, 'Toggle header name');
+		var testElement = React.createElement('div', {key: 'test'}, <Collection />);
+		if(this.state.isHidden) {
+			return React.createElement('div', null, [buttonElement, testElement]);
+		}
+		return React.createElement('div', null, [buttonElement, headerElement]);
 	}
 });
 
