bfa79d9b328803c37bdcb1f3670f193621255c15
HelloWorld20 <292701515@qq.com>
2016-09-07 23:31:52 +0800

    add npm jquery

--- reactjs/app/collection.react.js
+++ reactjs/app/collection.react.js
@@ -1,15 +1,32 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery');
 
 var Collection = React.createClass({
 	getInitialState: function() {
 		return {
-			title: 'title of collection.react.js'
+			title: 'title of collection.react.js',
+			message: 'default message'
 		}
 	},
-
+	clickAndAlert: function() {
+		var that = this;
+		$.ajax({
+			type: 'get',
+			url: 'http://localhost:3000/demo/demo/reactjs/source/test.json',
+			dataType: 'json',
+			success: function(res) {
+				that.setState({
+					message: res.key
+				})
+			}
+		})
+	},
 	render: function() {
-		return <h3>{this.state.title}</h3>;
+		return <div>
+			<a href="#">{this.state.message}#</a>
+			<h3 onClick={this.clickAndAlert}>{this.state.title}</h3>
+		</div>;
 	}
 })
 
